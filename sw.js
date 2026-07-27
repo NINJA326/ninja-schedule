fe7ec228f57b13bf85d023dc86eb2e3ad13ed4ea
NINJA326 <xxxmastsxxx@gmail.com>
2026-07-28 01:58:04 +0900

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,25 +1,30 @@
-const CACHE='ninja-schedule-v4';
-const STATIC_ASSETS=['./manifest.json'];
+const CACHE='ninja-schedule-v5';
+const STATIC_ASSETS=['./manifest.json','./icon.svg'];
+
 self.addEventListener('install',event=>{
   self.skipWaiting();
-  event.waitUntil(caches.open(CACHE).then(cache=>cache.addAll(STATIC_ASSETS)));
+  event.waitUntil(caches.open(CACHE).then(cache=>cache.addAll(STATIC_ASSETS)).catch(()=>{}));
 });
+
 self.addEventListener('activate',event=>{
-  event.waitUntil(Promise.all([
-    caches.keys().then(keys=>Promise.all(keys.filter(key=>key!==CACHE).map(key=>caches.delete(key)))),
-    self.clients.claim()
-  ]));
+  event.waitUntil((async()=>{
+    const keys=await caches.keys();
+    await Promise.all(keys.filter(key=>key!==CACHE).map(key=>caches.delete(key)));
+    await self.clients.claim();
+  })());
 });
+
 self.addEventListener('fetch',event=>{
   if(event.request.method!=='GET')return;
   const url=new URL(event.request.url);
-  if(url.origin===self.location.origin&&(url.pathname.endsWith('/')||url.pathname.endsWith('/index.html'))){
+  const isHtml=event.request.mode==='navigate'||url.pathname.endsWith('.html')||url.pathname.endsWith('/');
+  if(isHtml){
     event.respondWith(fetch(event.request,{cache:'no-store'}).catch(()=>caches.match('./index.html')));
     return;
   }
   event.respondWith(fetch(event.request).then(response=>{
     const copy=response.clone();
-    caches.open(CACHE).then(cache=>cache.put(event.request,copy));
+    caches.open(CACHE).then(cache=>cache.put(event.request,copy)).catch(()=>{});
     return response;
   }).catch(()=>caches.match(event.request)));
 });
