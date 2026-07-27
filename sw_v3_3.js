ceabad6007193706b6b2b9c496ddcdb569462e8c
NINJA326 <xxxmastsxxx@gmail.com>
2026-07-27 23:54:37 +0900

    Delete sw_v3_3.js

--- sw_v3_3.js
+++
@@ -1,5 +0,0 @@
-const CACHE='ninja-schedule-v3-3-line-fix';
-const ASSETS=['./','./index.html','./manifest.json'];
-self.addEventListener('install',e=>{self.skipWaiting();e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)))});
-self.addEventListener('activate',e=>e.waitUntil(Promise.all([self.clients.claim(),caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k))))])));
-self.addEventListener('fetch',e=>{if(e.request.method!=='GET')return;e.respondWith(fetch(e.request,{cache:'no-store'}).then(r=>{const x=r.clone();caches.open(CACHE).then(c=>c.put(e.request,x));return r}).catch(()=>caches.match(e.request)))});
