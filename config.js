49d01b9e7cd7b3021ebbcdd3e319790beff49517
Mitsuru Nakase <xxxmastsxxx@gmail.com>
2026-08-02 21:40:04 +0900

    Update API URL

--- config.js
+++ config.js
@@ -1,4 +1,5 @@
 'use strict';
+
 window.NINJA_SCHEDULE_CONFIG = Object.freeze({
-  API_URL: 'https://script.google.com/macros/s/AKfycbzm_oS0K-zuFubrtC6QE8O4JneXN2MMsBl4l8ZhH9tXvvDdRquPRCt7lD1I6L8_zaKPmA/exec'
+  API_URL: 'https://script.google.com/macros/s/AKfycbx_SBI38cJqdYtnIyeicdnpnTMu4wG67pak-tR3JwqyLHNoyUGxQ_NulmCJsxLF9_Q/exec'
 });
