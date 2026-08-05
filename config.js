7efc7aebc34ff753f6525ac8c6b62cccbc6dc180
NINJA326 <xxxmastsxxx@gmail.com>
2026-08-05 23:39:03 +0900

    Fix API_URL formatting in config.js

--- config.js
+++ config.js
@@ -1,5 +1,6 @@
 'use strict';
 
 window.NINJA_SCHEDULE_CONFIG = Object.freeze({
-  API_URL: 'https://script.google.com/macros/s/AKfycbx_SBI38cJqdYtnIyeicdnpnTMu4wG67pak-tR3JwqyLHNoyUGxQ_NulmCJsxLF9_Q/exec'
+  API_URL:
+    'https://script.google.com/macros/s/AKfycbx_SBI38cJqdYtnIyeicdnpnTMu4wG67pak-tR3JwqyLHNoyUGxQ_NulmCJsxLF9_Q/exec',
 });
